be4252c5a9f4848a0b90970697494b6c4d4dcc03
Max <burmistrovm@live.ru>
2021-06-02 18:42:42 +0300

    Fixed wrong agent port

--- src/components/dashboard/Terminal.vue
+++ src/components/dashboard/Terminal.vue
@@ -40,7 +40,7 @@ export default {
     //   }
     // });
 
-    const socket = new WebSocket(`ws://${this.terminalState.agentAddress}/tunnel?sid=${this.terminalState.sid}`);
+    const socket = new WebSocket(`ws://${this.terminalState.agentAddress}:11007/tunnel?sid=${this.terminalState.sid}`);
 
     socket.onclose = (event) => {
       this.terminal.write('\r\nConnection closed by remote host.');
